cfc7b3b02e07049ace3ec822d2f7f8d7abee0c8e
Martin Svejda <svejdam13@gmail.com>
2021-03-18 20:38:32 +0100

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+    </div>
+  )
+}
---
+++ src/pages/projects/test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>
+      That's how you make subdirectories : folder inside pages folder
+    </div>
+  )
+}
