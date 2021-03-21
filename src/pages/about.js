0bf9f1df4d3ec1212001c91f8a8e156dcd5b8983
Martin Svejda <svejdam13@gmail.com>
2021-03-21 09:15:59 +0100

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
   return (
-    <div>
-      <h1>About page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aspernatur.</p>
+      </div>
+    </Layout>
   )
 }
